d6848dea3be149fb3ccea3c4631844d96ee4aed4
SkyWace <simon.delcloque@hotmail.fr>
2023-05-11 10:09:08 +0200

    Change php

---
+++ PHP/ConnexionBDD.php
@@ -0,0 +1,14 @@
+<?php
+// Connexion à la base de données
+$host = 'localhost:8889';
+$dbname = 'phpvideo';
+$user = 'root';
+$password = 'root';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erreur de connexion à la base de données: " . $e->getMessage();
+}
+?>
