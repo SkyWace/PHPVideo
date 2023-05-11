bc871daec884a37c4e7e707aafab82d54a2f9a8e
SkyWace <simon.delcloque@hotmail.fr>
2023-05-11 10:48:30 +0200

    CSS delete

--- Index.php
+++ Index.php
@@ -6,7 +6,7 @@ require ('PHP/ConnexionBDD.php');
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Movflx - Online Movies & TV Shows Template</title>
+        <title>Movflx - Streaming, Emissions TV et +.</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -44,101 +44,9 @@ require ('PHP/ConnexionBDD.php');
             <i class="fas fa-angle-up"></i>
         </button>
         <!-- Scroll-top-end-->
-
-        <!-- header-area -->
-        <header>
-            <div id="sticky-header" class="menu-area transparent-header">
-                <div class="container custom-container">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="mobile-nav-toggler"><i class="fas fa-bars"></i></div>
-                            <div class="menu-wrap">
-                                <nav class="menu-nav show">
-                                    <div class="logo">
-                                        <a href="index.html">
-                                            <img src="img/logo/logo.png" alt="Logo">
-                                        </a>
-                                    </div>
-                                    <div class="navbar-wrap main-menu d-none d-lg-flex">
-                                        <ul class="navigation">
-                                            <li class="active menu-item-has-children"><a href="#">Home</a>
-                                                <ul class="submenu">
-                                                    <li class="active"><a href="index.html">Home One</a></li>
-                                                    <li><a href="index-2.html">Home Two</a></li>
-                                                </ul>
-                                            </li>
-                                            <li class="menu-item-has-children"><a href="#">Movie</a>
-                                                <ul class="submenu">
-                                                    <li><a href="movie.html">Movie</a></li>
-                                                    <li><a href="movie-details.html">Movie Details</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="contact.html">contacts</a></li>
-                                        </ul>
-                                    </div>
-                                    <div class="header-action d-none d-md-block">
-                                        <ul>
-                                            <li class="header-search"><a href="#" data-toggle="modal" data-target="#search-modal"><i class="fas fa-search"></i></a></li>
-                                            <li class="header-lang">
-                                                <form action="#">
-                                                    <div class="icon"><i class="flaticon-globe"></i></div>
-                                                    <select id="lang-dropdown">
-                                                        <option value="">En</option>
-                                                        <option value="">Fr</option>
-                                                    </select>
-                                                </form>
-                                            </li>
-                                            <li class="header-btn"><a href="model/User_update.php" class="btn">Sign In</a></li>
-                                        </ul>
-                                    </div>
-                                </nav>
-                            </div>
-
-                            <!-- Mobile Menu  -->
-                            <div class="mobile-menu">
-                                <div class="close-btn"><i class="fas fa-times"></i></div>
-
-                                <nav class="menu-box">
-                                    <div class="nav-logo"><a href="index.html"><img src="img/logo/logo.png" alt="" title=""></a>
-                                    </div>
-                                    <div class="menu-outer">
-                                        <!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header-->
-                                    </div>
-                                    <div class="social-links">
-                                        <ul class="clearfix">
-                                            <li><a href="#"><span class="fab fa-twitter"></span></a></li>
-                                            <li><a href="#"><span class="fab fa-facebook-square"></span></a></li>
-                                            <li><a href="#"><span class="fab fa-pinterest-p"></span></a></li>
-                                            <li><a href="#"><span class="fab fa-instagram"></span></a></li>
-                                            <li><a href="#"><span class="fab fa-youtube"></span></a></li>
-                                        </ul>
-                                    </div>
-                                </nav>
-                            </div>
-                            <div class="menu-backdrop"></div>
-                            <!-- End Mobile Menu -->
-
-                            <!-- Modal Search -->
-                            <div class="modal fade" id="search-modal" tabindex="-1" role="dialog" aria-hidden="true">
-                                <div class="modal-dialog" role="document">
-                                    <div class="modal-content">
-                                        <form>
-                                            <input type="text" placeholder="Search here...">
-                                            <button><i class="fas fa-search"></i></button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Modal Search-end -->
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </header>
-        <!-- header-area-end -->
-
-
+<?php
+    require ('PHP/header.php');
+?>
         <!-- main-area -->
         <main>
 
@@ -149,24 +57,24 @@ require ('PHP/ConnexionBDD.php');
                         <div class="col-xl-6 col-lg-8">
                             <div class="banner-content">
                                 <h6 class="sub-title wow fadeInUp" data-wow-delay=".2s" data-wow-duration="1.8s">Movflx</h6>
-                                <h2 class="title wow fadeInUp" data-wow-delay=".4s" data-wow-duration="1.8s">Unlimited <span>Movie</span>, TVs Shows, & More.</h2>
-                                <div class="banner-meta wow fadeInUp" data-wow-delay=".6s" data-wow-duration="1.8s">
+                                <h2 class="title wow fadeInUp" data-wow-delay=".4s" data-wow-duration="1.8s">Films <span>illimités</span>,Emisions TV & Ultra x.</h2>
+                                <div class="banner-meta wow fadeInUp" data-wow-delay=".6s" data-wow-duration="1s">
                                     <ul>
                                         <li class="quality">
-                                            <span>Pg 18</span>
+                                            <span>Pg 42</span>
                                             <span>hd</span>
                                         </li>
                                         <li class="category">
-                                            <a href="#">Romance,</a>
-                                            <a href="#">Drama</a>
+                                            <a href="#">CESI,</a>
+                                            <a href="#">DI2022</a>
                                         </li>
                                         <li class="release-time">
-                                            <span><i class="far fa-calendar-alt"></i> 2021</span>
-                                            <span><i class="far fa-clock"></i> 128 min</span>
+                                            <span><i class="far fa-calendar-alt"></i> 2022</span>
+                                            <span><i class="far fa-clock"></i> 2 Years</span>
                                         </li>
                                     </ul>
                                 </div>
-                                <a href="https://www.youtube.com/watch?v=R2gbPxeNk2E" class="banner-btn btn popup-video wow fadeInUp" data-wow-delay=".8s" data-wow-duration="1.8s"><i class="fas fa-play"></i> Watch Now</a>
+                                <a href="https://www.youtube.com/watch?v=_9oRPJqBv1s" class="banner-btn btn popup-video wow fadeInUp" data-wow-delay=".8s" data-wow-duration="1.8s"><i class="fas fa-play"></i> Watch Now</a>
                             </div>
                         </div>
                     </div>
@@ -208,7 +116,7 @@ require ('PHP/ConnexionBDD.php');
                                         <div class="bottom">
                                             <ul>
                                                 <li>
-                                                    <span class="duration"><i class="far fa-clock"></i><?=$duration?> min</span>
+                                                    <span class="duration"><i class="far fa-clock"></i><?=$duration?>    min</span>
                                                 </li>
                                             </ul>
                                         </div>
---
+++ PHP/header.php
@@ -0,0 +1,67 @@
+<header>
+            <div id="sticky-header" class="menu-area transparent-header">
+                <div class="container custom-container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="mobile-nav-toggler"><i class="fas fa-bars"></i></div>
+                            <div class="menu-wrap">
+                                <nav class="menu-nav show">
+                                    <div class="logo">
+                                        <a href="index.html">
+                                            <img src="img/logo/logo.png" alt="Logo">
+                                        </a>
+                                    </div>
+                                    <div class="navbar-wrap main-menu d-none d-lg-flex">
+                                        <ul class="navigation">
+                                            <li class="active menu-item-has-children"><a href="#">Home</a>
+                                                <ul class="submenu">
+                                                    <li class="active"><a href="index.html">Home One</a></li>
+                                                    <li><a href="index-2.html">Home Two</a></li>
+                                                </ul>
+                                            </li>
+                                            <li class="menu-item-has-children"><a href="#">Movie</a>
+                                                <ul class="submenu">
+                                                    <li><a href="movie.html">Movie</a></li>
+                                                    <li><a href="movie-details.php">Movie Details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="contact.html">contacts</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="header-action d-none d-md-block">
+                                        <ul>
+                                            <li class="header-btn"><a href="model/User_update.php" class="btn">Sign In</a></li>
+                                        </ul>
+                                    </div>
+                                </nav>
+                            </div>
+
+                            <!-- Mobile Menu  -->
+                            <div class="mobile-menu">
+                                <div class="close-btn"><i class="fas fa-times"></i></div>
+
+                                <nav class="menu-box">
+                                    <div class="nav-logo"><a href="index.html"><img src="img/logo/logo.png" alt="" title=""></a>
+                                    </div>
+                                    <div class="menu-outer">
+                                        <!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header-->
+                                    </div>
+                                    <div class="social-links">
+                                        <ul class="clearfix">
+                                            <li><a href="#"><span class="fab fa-twitter"></span></a></li>
+                                            <li><a href="#"><span class="fab fa-facebook-square"></span></a></li>
+                                            <li><a href="#"><span class="fab fa-pinterest-p"></span></a></li>
+                                            <li><a href="#"><span class="fab fa-instagram"></span></a></li>
+                                            <li><a href="#"><span class="fab fa-youtube"></span></a></li>
+                                        </ul>
+                                    </div>
+                                </nav>
+                            </div>
+                            <div class="menu-backdrop"></div>
+                            <!-- End Mobile Menu -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- header-area-end -->
