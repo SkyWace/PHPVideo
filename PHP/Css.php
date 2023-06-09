da1c07d1655a8de3c3a2782364a7fe7105dedfda
SkyWace <simon.delcloque@hotmail.fr>
2023-06-09 09:04:23 +0200

    CSS.php

--- PHP/Css.php
+++
@@ -1,8 +0,0 @@
-<link rel="stylesheet" href="../css/bootstrap.min.css">
-        <link rel="stylesheet" href="../css/animate.min.css">
-        <link rel="stylesheet" href="../css/magnific-popup.css">
-        <link rel="stylesheet" href="../css/fontawesome-all.min.css">
-        <link rel="stylesheet" href="../css/flaticon.css">
-        <link rel="stylesheet" href="../css/style.css">
-        <link rel="stylesheet" href="../css/responsive.css">
-        <link rel="stylesheet" href="../css/styleP.css">
